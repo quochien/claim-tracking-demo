14237dd5a562b99763d1dec9d6796d12c2761bf5
Hien Nguyen <quochien@gmail.com>
2019-09-17 12:41:03 +0700

    Update to use enumerize gem

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -2,7 +2,6 @@ class Claim < ApplicationRecord
   extend Enumerize
 
   validates :claim_number, :claimant_name, :email, :accident_date, :status, presence: true
-  # enum status: %I[in_progress evaluation approval rejection]
 
   enumerize :status, in: %I[in_progress evaluation approval rejection], default: :in_progress
 end
